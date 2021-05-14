26ebe533eec874fa06a7ab1d021a29279bcc3ef1
Aznaur <aznaur.khapchaev@gmail.com>
2021-05-14 18:38:36 +0300

    анимировал скрол

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,28 @@
 "use strict";
 {
   let scroll = document.querySelector('.greeting__scroll');
-  let about = document.querySelector('.about');
+  let about = document.querySelector('.about-company');
+  let btn = document.querySelector('.btnTop');
+  let header = document.querySelector('.page-header');
 
   scroll.addEventListener('click', function () {
     about.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+
+  window.onscroll = function () {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      btn.classList.add('btnActive');
+    } else {
+      btn.classList.remove('btnActive');
+    }
+  }
+
+  btn.addEventListener('click', function () {
+    header.scrollIntoView({
       behavior: 'smooth'
     });
   });
