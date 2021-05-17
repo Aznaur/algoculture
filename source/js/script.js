1df5efb29846b38c469c76b9bfead4048d7b22aa
Aznaur <aznaur.khapchaev@gmail.com>
2021-05-17 19:22:30 +0300

    Адаптив шапки

--- source/js/script.js
+++ source/js/script.js
@@ -3,7 +3,20 @@
   let scroll = document.querySelector('.greeting__scroll');
   let about = document.querySelector('.about-company');
   let btn = document.querySelector('.btnTop');
+  let navOpen = document.querySelector('.main-nav__toggle');
   let header = document.querySelector('.page-header');
+  let body = document.querySelector('body');
+  let mainNav = document.querySelector('.main-nav__list');
+  let socilaList = document.querySelector('.main-nav__social');
+
+  navOpen.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    navOpen.classList.toggle('main-nav__toggle--active');
+    socilaList.classList.toggle('main-nav__social--active');
+    mainNav.classList.toggle('main-nav__list--toggle');
+    header.classList.toggle('page-header--menu');
+    body.classList.toggle('body--scroll');
+  });
 
   scroll.addEventListener('click', function () {
     about.scrollIntoView({
