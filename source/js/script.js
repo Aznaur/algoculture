59f9e3b828becd656098f03b367d762df3d935b0
Aznaur <aznaur.khapchaev@gmail.com>
2021-05-20 02:29:43 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -9,6 +9,7 @@
   let mainNav = document.querySelector('.main-nav__list');
   let socilaList = document.querySelector('.main-nav__social');
 
+
   navOpen.addEventListener('click', function (evt) {
     evt.preventDefault();
     navOpen.classList.toggle('main-nav__toggle--active');
@@ -39,4 +40,42 @@
       behavior: 'smooth'
     });
   });
+
+  const animItems = document.querySelectorAll('.anim-items');
+
+  if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+
+    function animOnScroll () {
+      for (let index = 0; index < animItems.length; index++) {
+        const animItem = animItems[index];
+        const animItemHeight = animItem.offsetHeight;
+        const animItemOffset = offset(animItem).top;
+        const animStart = 4;
+
+        let animItemPoint = window.innerHeight - animItemHeight / animStart;
+        if (animItemHeight > window.innerHeight) {
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+        }
+
+        if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+          animItem.classList.add('active');
+        } else {
+          if (!animItem.classList.contains('anim-none')) {
+            animItem.classList.remove('active');
+          }
+        }
+      }
+    }
+
+    function offset(el) {
+      const rect = el.getBoundingClientRect();
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTo;
+      return {top: rect.top + scrollTop, left: rect.left + screenLeft};
+    }
+
+    setTimeout(() => {
+      animOnScroll();
+    }, 300);
+  }
 }
